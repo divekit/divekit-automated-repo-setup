658a7a198b8bad21070ac7d8449d157a00bd4fba
Stefan Bente <stefan.bente@th-koeln.de>
2023-06-01 15:18:07 +0200

    fixes #12

--- src/main/file_manipulator/UmletFileManipulator.ts
+++ src/main/file_manipulator/UmletFileManipulator.ts
@@ -1,9 +1,12 @@
-import { exec } from "node:child_process"
+import { promisify } from 'util';
+import { exec as execCb } from 'child_process';
+const exec = promisify(execCb);
 import { FileManipulator } from "./FileManipulator";
 import { RepositoryFile } from "../content_manager/RepositoryFile";
 import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 import * as path from 'path';
+import {Logger} from "../logging/Logger";
 
 export class UmletFileManipulator implements FileManipulator {
 
@@ -15,21 +18,26 @@ export class UmletFileManipulator implements FileManipulator {
 
     async manipulateRepositoryFile(repositoryFile: RepositoryFile): Promise<RepositoryFile[]> {
         let repositoryFiles: RepositoryFile[] = [];
-        repositoryFiles.push(repositoryFile);
+        try {
+            repositoryFiles.push(repositoryFile);
 
-        fs.mkdirSync(this.tmpUmletFolder, { recursive: true });
-        let fileId = uuidv4();
-        let rawFilePathTmp = path.join(this.tmpUmletFolder, `umlet_${fileId}.uxf`);
-        let imageFilePathTmp = path.join(this.tmpUmletFolder, `umlet_${fileId}.jpg`);
-        fs.writeFileSync(rawFilePathTmp, repositoryFile.content);
-        exec("umlet -action=convert -format=jpg -filename=" + rawFilePathTmp + " -output=" + imageFilePathTmp);
-        let imageFileContent = fs.readFileSync(imageFilePathTmp, { encoding: 'base64' });
-        let newRepositoryFile: RepositoryFile = { path: repositoryFile.path.replace("uxf", "jpg"), content: imageFileContent, encoding: "base64" };
-        repositoryFiles.push(newRepositoryFile);
+            fs.mkdirSync(this.tmpUmletFolder, { recursive: true });
+            let fileId = uuidv4();
+            let rawFilePathTmp = path.join(this.tmpUmletFolder, `umlet_${fileId}.uxf`);
+            let imageFilePathTmp = path.join(this.tmpUmletFolder, `umlet_${fileId}.jpg`);
+            fs.writeFileSync(rawFilePathTmp, repositoryFile.content);
+            await exec("umlet -action=convert -format=jpg -filename=" + rawFilePathTmp + " -output=" + imageFilePathTmp);
 
-        fs.unlinkSync(rawFilePathTmp);
-        fs.unlinkSync(imageFilePathTmp);
+            let imageFileContent = fs.readFileSync(imageFilePathTmp, { encoding: 'base64' });
+            let newRepositoryFile: RepositoryFile = { path: repositoryFile.path.replace("uxf", "jpg"), content: imageFileContent, encoding: "base64" };
+            repositoryFiles.push(newRepositoryFile);
 
+            fs.unlinkSync(rawFilePathTmp);
+            fs.unlinkSync(imageFilePathTmp);
+        } catch (error) {
+            Logger.getInstance().error(error, "Error while manipulating umlet file");
+            throw error;
+        }
         return repositoryFiles;
     }
 
